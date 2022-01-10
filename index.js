5ff042278d7af44c6a5f5568f0cd0e409642c42b
Arpan KC <nipeshkc7@gmail.com>
2022-01-10 20:34:08 +1100

    cli tool using inquirer

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
-console.log('Hello npm version 3!');
+#!/usr/bin/env node
 
-function helloNpm(){
-  console.log('Hello npm version 3!');
+const path = require('path');
+const inquirer = require('inquirer');
+
+function init() {
+
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "template",
+                message: `what template do you wanna use? `,
+                choices: templates
+            },
+            {
+                type: "question",
+                name: "projectName",
+                message: "Name of the project, default",
+                default: path.basename(path.resolve())
+            }
+        ])
+        .then((answers) => {
+
+            // const template = fs.readFileSync(path.join(__dirname, `/markdown-templates/${answers.template}`), 'utf8')
+            // const content = generate(template, answers);
+            // fs.writeFileSync(path.join(process.cwd(), '/README.md'), content);
+
+            console.log('Successfully generated README.md');
+        })
+        .catch((error) => {
+            console.error(`Error running process ${error.message}`);
+        });
+
 }
 
-module.exports = helloNpm;
+// function getTemplates() {
+//     return fs.readdirSync(path.join(__dirname, '/markdown-templates'));
+// }
+
+init();
