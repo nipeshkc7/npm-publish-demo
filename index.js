05cf7dc5f7858e58ddd7b678e7e7d326d0749909
Arpan KC <nipeshkc7@gmail.com>
2022-01-10 21:03:08 +1100

    updating index.js

--- index.js
+++ index.js
@@ -1,42 +1,23 @@
-#!/usr/bin/env node
-
-const path = require('path');
 const inquirer = require('inquirer');
 const axios = require('axios');
 
-function init() {
-
-    inquirer
-        .prompt([
-            // {
-            //     type: "list",
-            //     name: "template",
-            //     message: `what template do you wanna use? `,
-            //     choices: ['react', 'vue', 'angular']
-            // },
-            {
-                type: "question",
-                name: "pokemonName",
-                message: "Name of pokemon",
-                default: "pikachu" 
-            }
-        ])
-        .then((answers) => {
-
-            // const template = fs.readFileSync(path.join(__dirname, `/markdown-templates/${answers.template}`), 'utf8')
-            // const content = generate(template, answers);
-            // fs.writeFileSync(path.join(process.cwd(), '/README.md'), content);
-
-            console.log('Successfully generated README.md');
-        })
-        .catch((error) => {
-            console.error(`Error running process ${error.message}`);
-        });
-
-}
-
-// function getTemplates() {
-//     return fs.readdirSync(path.join(__dirname, '/markdown-templates'));
-// }
-
-init();
+inquirer
+  .prompt([
+    {
+      type: "question",
+      name: "pokemonName",
+      message: "Name of pokemon",
+      default: "pikachu"
+    }
+  ])
+  .then(async (answers) => {
+    try {
+      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${answers.pokemonName}`)
+      console.log(res.data.types.map(type => type.type.name));
+    } catch (err) {
+      console.log("Pokemon not found, try again");
+    }
+  })
+  .catch((error) => {
+    console.error(`Error running process ${error.message}`);
+  });
