fff540fdb4c5cbd4b337cd32621ad24e3d7036e6
Arpan KC <nipeshkc7@gmail.com>
2022-01-12 08:12:08 +1100

    Use async await

--- index.js
+++ index.js
@@ -3,24 +3,24 @@
 const inquirer = require('inquirer');
 const axios = require('axios');
 
-console.log("Hello npm user!");
-inquirer
-  .prompt([
-    {
-      type: "question",
-      name: "pokemonName",
-      message: "Name of pokemon",
-      default: "pikachu"
-    }
-  ])
-  .then(async (answers) => {
-    try {
-      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${answers.pokemonName}`)
-      console.log(res.data.types.map(type => type.type.name));
-    } catch (err) {
-      console.log("Pokemon not found, try again");
-    }
-  })
-  .catch((error) => {
-    console.error(`Error running process ${error.message}`);
-  });
+async function init(){
+  const answers = await inquirer
+    .prompt([
+      {
+        type: "question",
+        name: "pokemonName",
+        message: "Name of pokemon",
+        default: "pikachu"
+      }
+    ]);
+
+  try {
+    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${answers.pokemonName}`)
+    console.log(res.data.types.map(type => type.type.name));
+  } catch (err) {
+    console.log("Pokemon not found, try again");
+  }
+}
+
+init();
+
